9880d8ad4262e8d7b27009bb8e401f4f56c3913a
Pedro Wanderley <peu-zico>
2020-05-19 08:46:09 -0300

    user migration & model

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,15 +1,27 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('appointments') // diz que esse model será salvo na tabela 'appointments'
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
-  id: String;
+  id: string;
 
   @Column()
-  provider: String;
+  provider: string;
 
   @Column('timestamp with time zone') // se não for Postgres - colocar 'timestamp'
   date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
